20b2aca20e458af2166d491a35b5539a1860bb02
unknown <ufukeminkoroglu@gmail.com>
2019-07-20 13:51:12 -0500

    My sql homework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+I have been able to complete first 5 steps so far. I will update my homework tomorrow, after due date. Thanks 
+
 read me...
--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
--- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+/* 
+1. List the following details of each employee: 
+employee number, last name, first name, gender, and salary.
+*/
+
 SELECT *
 FROM employees
 
@@ -14,7 +18,9 @@ SELECT employees.emp_no,
   INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
   
   
- --2. List employees who were hired in 1986.
+/* 
+2. List employees who were hired in 1986. 
+*/
  
  SELECT hire_date
  FROM employees
@@ -22,10 +28,12 @@ SELECT employees.emp_no,
 	WHERE hire_date > '1986-01-01'
  	AND  hire_date < '1986-12-31' ;
 	
+	
 /*
 3. List the manager of each department with the following information: 
 department number, department name, the manager's employee number, last name, 
-first name, and start and end employment dates. */
+first name, and start and end employment dates. 
+*/
 
 SELECT * 
 FROM dept_manager
@@ -47,3 +55,37 @@ SELECT dept_manager.emp_no,
   FROM dept_manager 
   INNER JOIN employees ON dept_manager.emp_no = employees.emp_no
   INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
+  
+  
+/*
+4. List the department of each employee with the following information: 
+employee number, last name, first name, and department name. 
+*/
+
+SELECT *
+FROM departments
+
+SELECT *
+FROM dept_emp
+
+SELECT *
+FROM employees
+
+SELECT 
+employees.last_name,	
+employees.first_name,
+employees.emp_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN departments ON (dept_emp.dept_no = departments.dept_no)
+
+/* 5. List all employees whose first name is "Hercules" 
+and last names begin with "B."
+*/
+
+SELECT first_name, last_name
+FROM employees
+
+WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%'
+
